f421bf2582658d47e57d5fd1a4f5551575415f2c
Evan Kozliner <evankozliner@gmail.com>
2014-08-23 15:53:07 -0400

    admin router and login view added

--- routes.js
+++ routes.js
@@ -1,10 +1,12 @@
 var resume = require('./controllers/resumeController.js');
 var blog = require('./controllers/blogController.js');
 var projects = require('./controllers/projectsController.js');
+var admin = require('./controllers/adminController.js');
 
 module.exports = function(app) {
   app.get('/', projects.index);
   app.get('projects', projects.index);
   app.get('/resume', resume.index);
-  app.get('/blog', blog.index)
+  app.get('/blog', blog.index);
+  app.get('/login', admin.login);
 }
