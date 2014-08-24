b6b333aa533e94aafd4061425fe75ecbde7dc1c9
Evan Kozliner <evankozliner@gmail.com>
2014-08-24 14:32:17 -0400

    Quick fix for that obnoxious display issue on small devices

--- routes.js
+++ routes.js
@@ -6,19 +6,7 @@ var admin = require('./controllers/adminController.js');
 module.exports = function(app, passport) {
   var LocalStrategy = require('passport-local').Strategy;
   var User = require('./models/user.js')
-  //User.authenticate()
-  passport.use(new LocalStrategy(function(username, password, done) {
-    User.findOne({ username: username }, function(err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, user);
-    });
-  }));
+  passport.use(new LocalStrategy(User.authenticate()));
 
   passport.serializeUser(User.serializeUser());
   passport.deserializeUser(User.deserializeUser());
@@ -33,3 +21,16 @@ module.exports = function(app, passport) {
   }
   ));
 }
+
+// passport.use(new LocalStrategy(function(username, password, done) {
+//   User.findOne({ username: username }, function(err, user) {
+//     if (err) { return done(err); }
+//     if (!user) {
+//       return done(null, false, { message: 'Incorrect username.' });
+//     }
+//     if (!user.validPassword(password)) {
+//       return done(null, false, { message: 'Incorrect password.' });
+//     }
+//     return done(null, user);
+//   });
+// }));
