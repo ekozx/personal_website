5612998e99eeb3b51f8e15c028b2bc161d720cba
Evan Kozliner <evankozliner@gmail.com>
2014-09-01 15:56:40 -0400

    added the single page

--- models/post.js
+++ models/post.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var postSchema = mongoose.Schema({
   picture: String,
   title: String,
+  indexTitle: String,
   preview: String,
   body: String,
   created_at: {type: Date, default: Date.now}
