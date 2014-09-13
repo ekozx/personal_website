9699742996b229390c27d9de41122209b193e884
Evan Kozliner <evankozliner@gmail.com>
2014-09-13 19:45:54 -0400

    created a user, now I'm removing the registration code

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -4,20 +4,20 @@ exports.login = function(req, res) {
 exports.new = function(req, res) {
   res.render('new');
 };
-exports.register = function(req, res) {
-  res.render('register');
-};
-exports.createUser = function(req, res) {
-  var User = require('../models/user.js');
-  User.register(new User({ username : req.body.username }), req.body.password, function(err, user) {
-    if (err) {
-      return res.render('register', { user : user });
-    }
-    passport.authenticate('local')(req, res, function () {
-      res.redirect('/');
-    });
-  });
-};
+// exports.register = function(req, res) {
+//   res.render('register');
+// };
+// exports.createUser = function(req, res) {
+//   var User = require('../models/user.js');
+//   User.register(new User({ username : req.body.username }), req.body.password, function(err, user) {
+//     if (err) {
+//       return res.render('register', { user : user });
+//     }
+//     passport.authenticate('local')(req, res, function () {
+//       res.redirect('/');
+//     });
+//   });
+// };
 exports.createPost = function(req, res) {
   var Post = require('../models/post.js');
   console.log(req.body.post);
