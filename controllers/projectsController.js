97146eafb7d16fed3e1571bc7517b66d6f203ed3
Evan Kozliner <evankozliner@gmail.com>
2014-09-22 11:23:22 -0400

    small change to avoid 404s

--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -2,27 +2,10 @@ var Post = require('../models/post.js');
 
 exports.index = function(req, res) {
   posts = [];
-  Post.findOne({indexTitle: "hello-world"}, function(err, post) {
+  Post.findOne({indexTitle: "Zapcord"}, function(err, post) {
     if (post != null) {
-      console.log(post);
       posts.push(post);
-      Post.findOne({indexTitle: "A-longer post"}, function(err, post) {
-        if (post != null) {
-          posts.push(post);
-          Post.findOne({indexTitle: "Zapcord"}, function(err, post) {
-            if (post != null) {
-              posts.push(post);
-              res.render('index', { posts: posts })
-            } else {
-              console.log("null post")
-              res.redirect('/404');
-            }
-          });
-        } else {
-          console.log("null post")
-          res.redirect('/404');
-        }
-      });
+      res.render('index', { posts: posts })
     } else {
       console.log("null post")
       res.redirect('/404');
