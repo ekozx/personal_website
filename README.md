537c748768d2af16789d11acd4e5c6d1f81a532a
Evan Kozliner <evankozliner@gmail.com>
2015-01-12 13:26:34 -0500

    Changed projects controller lookup function and readme

--- README.md
+++ README.md
@@ -6,5 +6,6 @@ To self: Don't forget to concatenate your css files!!
 * Add prior/upcoming workplaces to resume
 * Fix text size on layout.jade for mobile view
 * create mayan numerical project/post
+* Fix spelling errors (base bones in the processing one... Come on)
 * Add processing.js projects/post
   * This requires a new audio solution as Maxim.js appears to be deprecated
--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -1,16 +1,21 @@
 var Post = require('../models/post.js');
 
 exports.index = function(req, res) {
+  postNames = ["Zapcord", "Visual-Computing-with-Processing"];
   posts = [];
-  Post.findOne({indexTitle: "Zapcord"}, function(err, post) {
-    if (post != null) {
-      posts.push(post);
-      res.render('index', { posts: posts });
-    } else {
-      console.log("null post");
-      res.redirect('/404');
-    }
-  });
+  for (var i = 0; i < postNames.length; i++) {
+    Post.findOne({indexTitle: postNames[i]}, function(err, post) {
+      console.log(post);
+      if(post != null) {
+        posts.push(post);
+      } else {
+        res.render('/404');
+      }
+      if(posts.length === postNames.length) {
+        res.render('index', {posts: posts});
+      }
+    });
+  }
 };
 exports.processing = function(req, res) {
   res.render('processing');
